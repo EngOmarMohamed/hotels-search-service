c99fb761422bc24fd5102965f80f72403bd52772
EngOmarMohamed <o.mohamed10@gmail.com>
2018-03-25 02:00:20 +0200

    Test HotelRepository in ExampleController

--- app/Http/Controllers/ExampleController.php
+++ app/Http/Controllers/ExampleController.php
@@ -2,17 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use Service\Repository\HotelRepository;
+use Illuminate\Http\Request;
+
 class ExampleController extends Controller
 {
+    private $hotelRepo;
+
+
     /**
      * Create a new controller instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(HotelRepository $hotelRepository)
     {
-        //
+        $this->hotelRepo = $hotelRepository;
     }
 
-    //
+    public function index(Request $request)
+    {
+
+        $HotelsAsStr = $this->hotelRepo->get()
+            ->map(function ($hotel) {
+                return $hotel;
+            });
+
+        return $HotelsAsStr;
+
+    }
 }
